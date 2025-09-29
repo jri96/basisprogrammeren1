38cca1dc12a36af7e5e01de81b623d8bd66bd56b
Dennis van Wakeren <dennisvanwakeren@112366.local>
2025-09-29 09:29:55 +0200

    De opdracht van week 4

---
+++ week-04/1Naming.py
@@ -0,0 +1,41 @@
+# User_name | variabele | snake_case
+
+# last-name  | variabele | snake_case
+
+# HTTPresponseCode | variabele | SCREAMING_SNAKE_CASE (const)
+
+# total-price | variabele | snake_case
+
+# i | variabele | camelCase (geef betekenis)
+
+# birthDate | variabele | snake_case
+
+# MAX_SPEED | variabele | SCREAMING_SNAKE_CASE (const)
+
+# file2path | variabele | snake_case
+
+# tempCelsius | variabele | snake_case
+
+# Calculate_total | functie | snake_case
+
+# get-user id | functie | camelCase
+
+# ParseXML | functie | camelCase
+
+# isValidPassword | functie | camelCase
+
+# sendHTTPRequest | functie | snake_case
+
+# ConvertToCsv | functie | camelCase
+
+# usermanager | class | PascalCase
+
+# XML_parser | class | PascalCase
+
+# Student_grade | class | PascalCase
+
+# math-utils | class | PascalCase
+
+# gameEngine | class | PascalCase
+
+# apiClient | class | PascalCase
